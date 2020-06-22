a9dd49fdd730396d3bf10d64bcf5e59d6cc8e502
Kirby Wu <tkirby@gmail.com>
2020-06-22 14:14:38 +0800

    rebuild all html for navtop change

--- static/js/site.js
+++ static/js/site.js
@@ -14,6 +14,18 @@
     auth = arg$.auth, navtop = arg$.navtop;
     console.log("site general ldc");
     smoothScroll();
+    auth.get().then(function(g){
+      var view;
+      return view = new ldView({
+        global: true,
+        root: document.body,
+        text: {
+          "brand-name": function(){
+            return g.scope.teamname;
+          }
+        }
+      });
+    });
     if (typeof moment != 'undefined' && moment !== null) {
       return moment.tz.add(["Asia/Taipei|CST JST CDT|-80 -90 -90|01020202020202020202020202020202020202020|-1iw80 joM0 1yo0 Tz0 1ip0 1jX0 1cN0 11b0 1oN0 11b0 1oN0 11b0 1oN0 11b0 10N0 1BX0 10p0 1pz0 10p0 1pz0 10p0 1db0 1dd0 1db0 1cN0 1db0 1cN0 1db0 1cN0 1db0 1BB0 ML0 1Bd0 ML0 uq10 1db0 1cN0 1db0 97B0 AL0|74e5"]);
     }
