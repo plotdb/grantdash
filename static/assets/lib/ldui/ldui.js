b78e029d5046b0a884a7465084eb4c6e8e98a58b
Kirby Wu <tkirby@gmail.com>
2020-07-24 21:57:39 +0800

    update ldui lib

--- static/assets/lib/ldui/ldui.js
+++ static/assets/lib/ldui/ldui.js
@@ -10289,7 +10289,7 @@ var slice$ = [].slice;
           return results$;
         }()).join('&'));
       }
-      if (ld$.fetch.headers) {
+      if (ld$.fetch.headers && !p.noDefaultHeaders) {
         import$(c.headers || (c.headers = {}), ld$.fetch.headers);
       }
       return {
