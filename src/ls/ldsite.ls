c9718447224e995eb65db2935c629c0a822b4ba2
Kirby Wu <tkirby@gmail.com>
2020-07-25 19:17:02 +0800

    enable consent

--- src/ls/ldsite.ls
+++ src/ls/ldsite.ls
@@ -3,12 +3,10 @@
   ldc.register \ldsite, <[]>, ->
     return {
       api: \/dash/api
-      /*
       consent: tos: do
         type: \link
         url: \/consent.pdf
         timing: <[prj-create signin]>
-      */
       ldcvmgr-root: \/dash/modules/cover
       avatar-url: -> "/dash/s/avatar/#it.png"
     }
--- static/js/ldsite.js
+++ static/js/ldsite.js
@@ -2,13 +2,14 @@
 (function(){
   return ldc.register('ldsite', [], function(){
     return {
-      api: '/dash/api'
-      /*
-      consent: tos: do
-        type: \link
-        url: \/consent.pdf
-        timing: <[prj-create signin]>
-      */,
+      api: '/dash/api',
+      consent: {
+        tos: {
+          type: 'link',
+          url: '/consent.pdf',
+          timing: ['prj-create', 'signin']
+        }
+      },
       ldcvmgrRoot: '/dash/modules/cover',
       avatarUrl: function(it){
         return "/dash/s/avatar/" + it + ".png";
