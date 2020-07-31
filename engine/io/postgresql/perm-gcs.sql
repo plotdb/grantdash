e1dfdd7ebf69d6c00205e1154635dbf05feec489
Kirby Wu <tkirby@gmail.com>
2020-07-31 10:04:21 +0800

    add brd and grp slug in perm-gcs

--- engine/io/postgresql/perm-gcs.sql
+++ engine/io/postgresql/perm-gcs.sql
@@ -1,6 +1,9 @@
 create table if not exists perm_gcs (
   key serial primary key,
   owner int references users(key) not null,
+  brd text,
+  grp text,
   id text not null
 );
 
+create unique index if not exists perm_gcs_index on perm_gcs (id);
