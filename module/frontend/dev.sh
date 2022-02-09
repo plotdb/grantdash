4b4b00a74579fc8e35c170a159f9915802542d7e
Kirby Wu <tkirby@gmail.com>
2022-02-09 22:50:06 +0800

    add dev script for installing local dev modules; add @makeform blocks

---
+++ module/frontend/dev.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+modules="@taiccadash/base @taiccadash/future-content @taiccadash/vr @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload"
+base=~/workspace/zbryikt/grantdash/sites/block/static/block/
+
+param= 
+for module in $modules
+do
+  param="${param};$module:${base}$module/main"
+done
+param=${param:1}
+
+npx fedep -l $param -s false
