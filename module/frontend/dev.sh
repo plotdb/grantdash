4a9317029c75868c82c3a21b976bc21664ae1fd6
Kirby Wu <tkirby@gmail.com>
2022-02-14 00:46:14 +0800

    add future-content-111

--- module/frontend/dev.sh
+++ module/frontend/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-modules="@taiccadash/base @taiccadash/future-content @taiccadash/vr @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown"
+modules="@taiccadash/base @taiccadash/future-content @taiccadash/vr @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown"
 base=~/workspace/zbryikt/grantdash/sites/block/static/block/
 
 param= 
