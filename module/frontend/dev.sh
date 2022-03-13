ab03ef5bd9864a5dea92b14e697ab1a90a8b5870
Kirby Wu <tkirby@gmail.com>
2022-03-13 21:12:52 +0800

    update modules

--- module/frontend/dev.sh
+++ module/frontend/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-modules="@taiccadash/base @taiccadash/future-content @taiccadash/icg-111 @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown @makeform/checkbox @makeform/date @makeform/bilingual"
+modules="@taiccadash/base @taiccadash/future-content @taiccadash/icg-111 @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown @makeform/checkbox @makeform/date @makeform/bilingual @makeform/agreement"
 base=~/workspace/zbryikt/grantdash/sites/block/static/block/
 
 param= 
@@ -11,3 +11,4 @@ done
 param=${param:1}
 
 npx fedep -l $param -s false
+
