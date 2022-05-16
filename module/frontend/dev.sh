18a7ef6a9b75b32b55d90c48427b876735e32fa7
Kirby Wu <tkirby@gmail.com>
2022-05-16 14:20:07 +0800

    add new modules and upgrade @makeform widgets

--- module/frontend/dev.sh
+++ module/frontend/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-modules="@grantdash/qualification @taiccadash/qual @grantdash/prj.base @taiccadash/base @taiccadash/future-content @taiccadash/icg-111 @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown @makeform/checkbox @makeform/date @makeform/bilingual @makeform/agreement @makeform/common"
+modules="@taiccadash/fcm-111 @makeform/checklist @makeform/datetime @makeform/section @grantdash/qualification @taiccadash/qual @grantdash/prj.base @taiccadash/base @taiccadash/future-content @taiccadash/icg-111 @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown @makeform/checkbox @makeform/date @makeform/bilingual @makeform/agreement @makeform/common"
 base=~/workspace/zbryikt/grantdash/sites/block/static/block/
 
 param= 
