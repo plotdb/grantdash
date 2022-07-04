b759ebfc4734bfc78fbad059704b6e8b4441934e
zbryikt <tkirby@gmail.com>
2022-07-04 23:37:18 +0800

    add icg-110

--- module/frontend/dev.sh
+++ module/frontend/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-modules="@taiccadash/fcm-111 @makeform/checklist @makeform/datetime @makeform/section @grantdash/qualification @taiccadash/qual @grantdash/prj.base @taiccadash/base @taiccadash/future-content @taiccadash/icg-111 @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown @makeform/checkbox @makeform/date @makeform/bilingual @makeform/agreement @makeform/common"
+modules="@taiccadash/icg-110 @taiccadash/fcm-111 @makeform/checklist @makeform/datetime @makeform/section @grantdash/qualification @taiccadash/qual @grantdash/prj.base @taiccadash/base @taiccadash/future-content @taiccadash/icg-111 @taiccadash/future-content-111 @makeform/base @makeform/input @makeform/textarea @makeform/choice @makeform/radio @makeform/table @makeform/upload @makeform/countdown @makeform/checkbox @makeform/date @makeform/bilingual @makeform/agreement @makeform/common"
 if [ -d ~/workspace/zbryikt/grantdash/sites/block/static/block/ ]; then
   base=~/workspace/zbryikt/grantdash/sites/block/static/block/
 else
---
+++ src/pug/view/taicca/icg-110/block.ls
@@ -0,0 +1,12 @@
+(o) <- ldc.register <[blockbase viewLocals auth ldNotify error notify viewmode]>, _
+{blockbase, viewLocals, auth, error, notify, viewmode} = o
+if /judge/.exec(document.referrer or '') =>
+  blockdef = name: '@taiccadash/icg-110', version: 'main'
+else
+  blockdef = name: '@taiccadash/icg-110', version: 'main'
+brd = "icg-111"
+data = {mode: viewmode, is-embedded: if viewmode == \view => true else false}
+blockbase.init {blockdef, brd, data}
+  .catch ->
+    console.log 'failed: ', it
+    return Promise.reject it
